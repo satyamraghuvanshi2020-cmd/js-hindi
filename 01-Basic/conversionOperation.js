6144fa85866f0e8aa7b78907249e7e7947a48862
Satyam Raghuvanshi <satyamraghuvanshi2020@gmail.com>
2026-07-16 08:14:30 +0000

    conversion and operation

---
+++ 01-Basic/conversionOperation.js
@@ -0,0 +1,61 @@
+let score = true
+
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof(valueInNumber));
+//console.log(valueInNumber);
+
+let isLoggedIn=1
+let booleanLoggedIn=Boolean(isLoggedIn)
+console.log(booleanLoggedIn);
+
+let number=33
+let stringnumber=String(number)
+console.log(stringnumber);
+console.log(typeof(stringnumber));
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+ console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
