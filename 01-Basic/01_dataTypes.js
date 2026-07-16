b7b4f5f2ab9be2a05cf670b6e357f3d8065574ae
Satyam Raghuvanshi <satyamraghuvanshi2020@gmail.com>
2026-07-16 08:14:52 +0000

    conversion and operation

---
+++ 01-Basic/01_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as a newer version
+
+//alert(3+3) //we are using node js not browser
+console.log(3+
+    3) //code readability should be high
+console.log("Hitesh")
+
+let name = "Satyam"
+let age = 28
+let IsloggedIN = false
+
+//number => 2 to power 53
+//bigint
+//string = ""
+//boolean=true/false
+//null=standalone value
+//undefined
+//object
+console.log(typeof age)
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
