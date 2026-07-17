c9351f2bdabe695a857c17c7201b894d38a0bd99
Satyam Raghuvanshi <satyamraghuvanshi2020@gmail.com>
2026-07-17 07:06:22 +0000

    datasummary

---
+++ 01-Basic/datatypes_summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 type : String , Number , null , undefined, boolean, Symbol, BigInt
+
+const score=100
+const floatscore=100.03
+const isLoggedIn=true
+const outtemp=null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+const sign=Symbol("Red")
+//console.log(typeof id);
+//console.log(sign);
+
+//const bigNumber =23423245245254n
+// Non Primitive (Reference)
+
+// 3type : Array, Object , Function
+const Heroes=["superman","shaktiman","himan"]
+let obj={
+    name: "satyam",
+    age: "28",
+}
+
+const myfunction=function(){
+    console.log("function method");
+    
+}
+console.log(typeof bigNumber);
+console.log(typeof Heroes);
+console.log(typeof obj);
+console.log(typeof myfunction);
+console.log(typeof score);
+console.log(typeof floatscore);
+console.log(typeof bigNumber);
+console.log(typeof isLoggedIn);
+console.log(typeof outtemp);
+
+
