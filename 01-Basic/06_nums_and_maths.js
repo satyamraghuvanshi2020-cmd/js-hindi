619629519673d8e9c055a1d08f6d9449e940390c
Satyam Raghuvanshi <satyamraghuvanshi2020@gmail.com>
2026-07-18 08:28:06 +0000

    num_and_maths

---
+++ 01-Basic/06_nums_and_maths.js
@@ -0,0 +1,33 @@
+const score = 400
+
+//console.log(score);
+
+const balance = new Number(33);
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(3));
+
+const otherNumber = 123.345
+//console.log(otherNumber.toPrecision(2));
+
+const hundereds= 1000000
+//console.log(hundereds.toLocaleString(`en-In`));
+
+// ------------ Maths ---------------- //
+
+// console.log(Math);
+// console.log(Math.abs(-454));
+// console.log(Math.round(3.54));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(3.1));
+// console.log(Math.min(3,4,5,1,0.4,2));
+// console.log(Math.max(4,345,3425,243,12,0));
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log((Math.random()*10)+1);
+const min =10
+const max=20
+console.log(Math.floor(Math.random() * (max - min + 1))+min)
